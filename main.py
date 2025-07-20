0c604e1d433d40b20609a8d162ad0557aca65011
md-mejbaul-haque <mejba.s@gmail.com>
2025-07-21 03:19:52 +0600

    check marks added

--- main.py
+++ main.py
@@ -46,6 +46,11 @@ def count_down(count):
         window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -69,7 +74,7 @@ start_button.grid(column=0, row=2)
 reset_button =Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
-check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 
